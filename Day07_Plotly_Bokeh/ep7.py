5c7246c606a2f3393b6ab76f5710afa3ec056c2e
Tanawat Saitin <gnaraowh@gmail.com>
2020-11-09 12:45:00 +0700

    commit

--- Day07_Plotly_Bokeh/ep7.py
+++ Day07_Plotly_Bokeh/ep7.py
@@ -1,123 +1,9 @@
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import numpy as np
 import pandas as pd
 import os
-
-
-def getFilePath(file_name):
-    file_path = os.path.abspath(__file__)
-    dir_path = os.path.dirname(file_path)
-    root_path = os.path.dirname(dir_path)
-    data_path = os.path.join(root_path, '__Data')
-    ret_path = os.path.join(data_path, file_name)
-
-    return ret_path
-
-
-# rows = ['A', 'B', 'C', 'D']
-# cols = ['Score1', 'Score2', 'Midterm', 'Final']
-# values = np.random.randint(40, 100+1, (4, 4))
-# df = pd.DataFrame(values, rows, cols)
-# fig = px.bar(df, x=df.index, y='Midterm', hover_data=['Score1', 'Score2'], color='Final', labels={
-#              'index': 'ชื่อนักเรียน', 'Final': R'คะแนนสอบปลายภาค'})
-# fig.update_layout(xaxis_tickangle=-45)
-# fig.show()
-try:
-    df = pd.read_csv(
-        'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/Automobile_data.csv', na_values={
-            'normalized-losses': ['?'],
-            'strok': ['?'],
-            'horsepower': ['?'],
-            'num-of-doors': ['?'],
-            'peak-rpm': ['?'],
-            'price': ['?']
-        }
-    )
-except Exception as e:
-    df = pd.read_csv(getFilePath('Automobile_data.csv'), na_values={
-        'normalized-losses': ['?'],
-        'strok': ['?'],
-        'horsepower': ['?'],
-        'num-of-doors': ['?'],
-        'peak-rpm': ['?'],
-        'price': ['?']
-        }
-    )
-# df['price'] = pd.to_numeric(df['price'])
-# df['horsepower'] = pd.to_numeric(df['horsepower'])
-# fig = px.line(df, x='horsepower', y='price',title='Avg of Horsepower')
-# fig.show()
-
-#df2 = df.groupby('horsepower').mean()
-# fig = px.line(df2, x=df2.index, y='price',title='Avg of Horsepower')
-# fig.show()
-
-# df_ex = px.data.gapminder().query("continent=='Oceania'")
-# fig = px.line(df_ex, x='year', y='lifeExp', color='country')
-# fig.show()
-
-# df2 = df.groupby('make').std()
-# print(df2)
-# fig = px.pie(df2, values='price', names=df2.index, title='Price')
-# fig.show()
-
-# df2 = df.groupby('body-style').std()
-# df2.head()
-# print(df2)
-# fig = px.pie(df2, values='horsepower', names=df2.index, title='std Price', color_discrete_sequence=px.colors.sequential.Purp)
-# fig.show()
-
-# df2 = df.groupby('horsepower').mean()
-# df2.head()
-# fig = px.scatter(df2, x=df2.index, y='price', size='engine-size', color_discrete_sequence=px.colors.cyclical.HSV, color='wheel-base',
-#                  labels={'horsepower': 'แรงม้า', 'price': 'ราคาเฉลี่ย', 'engine-size': 'ขนาดเครื่องยนต์'})
-# fig.show()
-
-# fig = px.box(df.dropna(), x='body-style', y='price', points='all', color='num-of-doors')
-# fig.show()
-
-# df2 = df.corr()
-# fig = go.Figure(data=go.Heatmap(z=df2, x=df2.index,
-#                                 y=df2.index, colorscale='Viridis'))
-# fig.show()
-
-# df3 = df.pivot_table(index='body-style',columns='drive-wheels', values='price')
-# fig = go.Figure(data=go.Heatmap(z=df3,
-#                                 x=df3.columns,
-#                                 y=df3.index, colorscale='Picnic'))
-# fig.show()
-
-# fig = px.choropleth(locations=['CA','TX','NY'],locationmode='USA-states', color=[1,2,3], scope='usa')
-# fig.show()
-
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')#export argiculture of usa
-# df.head()
-# for col in df.columns:
-#     df[col] = df[col].astype(str)
-# df['text'] = df['state'] + '<br>'\
-#     +'beef ' + df['beef'] + ' Diary ' + df['dairy'] + '<br>'\
-#     +'Fruits ' + df['total fruits'] + ' Veggies ' + df['total veggies'] + '<br>'\
-#     +'Wheat ' + df['wheat'] + ' Corn ' + df['corn']
-# fig = go.Figure(data=go.Choropleth(
-#     locations=df['code']
-#     , z= df['total exports']
-#     , locationmode = 'USA-states'
-#     , colorscale = 'Picnic'
-#     , colorbar_title = 'Millions USD'
-#     , text=df['text']
-# ))
-# fig.update_layout(title_text='2011 US exports',geo_scope='usa')
-# fig.show()
-
-# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')#export argiculture of usa
-# fig = go.Figure(data=go.Choropleth(
-#     locations=df['CODE']
-#     , z= df['GDP (BILLIONS)']
-#     , colorscale = 'Picnic'
-#     , text=df['COUNTRY']
-# ))
-# fig.show()
 
 pd.set_option('display.max_rows', None, 'display.max_columns', None)
 
@@ -134,15 +20,14 @@ for i in range(0, len(rows)):
         v_rows.append(salary)
     values.append(v_rows)
 df = pd.DataFrame(values, rows, cols)
-#print(df)
+print(df)
 
 #---------------------------------------- QUIZ2 ----------------------------------------#
 df2 = pd.DataFrame.copy(df)
 df2['2017'] = df2['2017']*2
-#print(df2)
 fig = px.bar(df2, x=df2['2017'], y=df2.index)
 fig.update_layout(xaxis_tickangle=-45)
-# fig.show()
+fig.show()
 
 #---------------------------------------- QUIZ3 ----------------------------------------#
 cols = ['2016', '2017', '2018', '2019', '2020','mean']
@@ -151,8 +36,185 @@ df3['mean'] = df3.mean(axis=1)
 df3['2017'] = df3['2017']*2
 fig = px.bar(df3, x=df3['2017'], y=df3.index, hover_data=cols, labels={'index':'ชื่อ','2017':'เงินเดือนปี 2017 x 2'},color='2016')
 fig.update_layout(xaxis_tickangle=-45)
-#fig.show()
+fig.show()
 
-#---------------------------------------- QUIZ3 ----------------------------------------#
+#---------------------------------------- QUIZ4 ----------------------------------------#
 df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.dropna(subset=['Age'])
+df = df.groupby('Age').mean()
+fig = px.line(df, x=df.index, y='Fare',title='Average Fare group by Age', labels={'Age':'Age','Fare':'Avg Fare'})
+fig.show()
+
+#---------------------------------------- QUIZ5 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.groupby('Pclass').mean()
+fig = px.line(df, x=df.index, y='Fare',title='Average Fare group by Pclass', labels={'Pclass':'Pclass','Fare':'Avg Fare'})
+fig.show()
+
+#---------------------------------------- QUIZ6 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.dropna(subset=['Age'])
+df = df.groupby('Age').sum()
+fig = px.pie(df, values='Fare', names=df.index)
+
+#---------------------------------------- QUIZ7 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.dropna(subset=['Age'])
+df = df.groupby('Age').mean()
+fig = px.pie(df, values='Fare', names=df.index)
+
+#---------------------------------------- QUIZ8 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+sr_lastname = df['Name'].apply(lambda x: x.split(',')[0])
+df['Lastname'] = sr_lastname
+df = df.groupby('Lastname').describe()
+df = df['Fare']
+df = df[df['count']>1]
+fig = px.pie(df, values='mean', names=df.index)
+fig.show()
+
+#---------------------------------------- QUIZ9 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+sr_lastname = df['Name'].apply(lambda x: x.split(',')[0])
+df['Lastname'] = sr_lastname
+df = df.groupby('Lastname').describe()
+df = df['Age']
+df = df[df['count']>1]
 print(df)
+fig = px.pie(df, values='mean', names=df.index, title='Fare\'s Mean of Duplicate Lastname', color_discrete_sequence=px.colors.sequential.Purp)
+fig.show()
+
+#---------------------------------------- QUIZ10 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.groupby('Embarked').mean()
+
+titles = list(df.columns)
+labels = list(df.index)
+values = list(df['Fare'].values)
+num_graph = len(df.columns)
+
+tmp = []
+domains = []
+for i in range(0, len(df.columns)):
+    tmp.append({'type': 'domain'})
+    domains.append({'x': [0.0+0.14*i, 0.33], 'y': [0.0, 0.33]})
+specs = []
+specs.append(tmp)
+
+fig = make_subplots(rows=1, cols=num_graph, specs=specs, subplot_titles=titles)
+for i in range(0, len(titles)):
+    values = list(df[titles[i]].values)
+    fig.add_trace(go.Pie(labels=labels, values=values, scalegroup='one',
+                         name=titles[i]), 1, i+1)
+fig.show()
+
+#---------------------------------------- QUIZ11 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.groupby('Age').mean()
+fig = px.scatter(df, x=df.index, y='Fare', size='SibSp', color_discrete_sequence=px.colors.cyclical.HSV, color='Pclass',
+                 labels={'Age': 'อายุ', 'Fare': 'ราคาเฉลี่ย', 'SibSp': 'พี่น้อง/คู่สมรส'}, title='ราคาเฉลี่ย แยกตามอายุ ขนาดตาม พี่น้อง/คู่สมรส')
+fig.show()
+
+#---------------------------------------- QUIZ12 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+fig = px.box(df, x='Sex',y='Fare')
+fig.show()
+
+#---------------------------------------- QUIZ13 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+fig = px.box(df, x='Pclass',y='Fare')
+fig.show()
+
+#---------------------------------------- QUIZ14 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+fig = px.box(df, x='Pclass',y='Age', points='all')
+fig.show()
+
+#---------------------------------------- QUIZ15 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+fig = px.box(df, x='Survived',y='Age',color_discrete_sequence=px.colors.cyclical.HSV, color='Sex')
+fig.show()
+
+#---------------------------------------- QUIZ16 ----------------------------------------#
+# โจทย์ ถามหาจุดหมายปลายทางซึ่งไม่มีใน data เลยใส่ ข้อมูล Embarked ขึ้นเรื่อจากท่า แทน
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.groupby('Embarked').std()
+fig = px.bar(df, x=df.index,y='Fare')
+fig.show()
+
+#---------------------------------------- QUIZ17 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df = df.groupby('Pclass').mean()
+fig = px.bar(df, x=df.index,y='Age')
+fig.show()
+
+#---------------------------------------- QUIZ18 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df2 = df.pivot_table(index='Pclass', columns='Sex', values='Fare')
+fig = go.Figure(data=go.Heatmap(z=df2,x=df2.columns,y=df2.index, colorscale='Picnic'))
+fig.show()
+
+#---------------------------------------- QUIZ19 ----------------------------------------#
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/gnaraowh/Homework_DataCamp1/master/__Data/train.csv')
+df2 = df.corr()
+fig = go.Figure(data=go.Heatmap(z=df2,x=df2.index,y=df2.index, colorscale='Picnic'))
+fig.show()
+
+#---------------------------------------- QUIZ20 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+df.head()
+for col in df.columns:
+    df[col] = df[col].astype(str)
+df['text'] = df['state'] + '<br>'\
+    +'total exports ' + df['total exports'] + '<br>'\
+    +'total fruits ' + df['total fruits'] + '<br>'\
+    +'total veggies ' + df['total veggies']
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code']
+    , z= df['cotton']
+    , locationmode = 'USA-states'
+    , colorscale = 'Picnic'
+    , colorbar_title = 'Millions USD'
+    , text=df['text']
+))
+fig.update_layout(title_text='2011 US exports',geo_scope='usa')
+fig.show()
+
+#---------------------------------------- QUIZ21 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+df['beef:pork'] = round(df['beef']/df['pork'],2)
+for col in df.columns:
+    df[col] = df[col].astype(str)
+df['text'] = df['state'] + '<br>'\
+    +'beef:pork ' + df['beef:pork']
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code']
+    , z= df['total veggies']
+    , locationmode = 'USA-states'
+    , colorscale = 'Picnic'
+    , colorbar_title = 'Millions USD'
+    , text=df['text']
+))
+fig.update_layout(title_text='2011 US exports',geo_scope='usa')
+fig.show()
+
+#---------------------------------------- QUIZ22 ----------------------------------------#
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+df = df.sample(n=10)
+df['cotton:wheat'] = df['cotton']/df['wheat']
+df['cotton:wheat'] = df['cotton:wheat'].fillna(value=0)
+fig = px.bar(df, x=df['state'], y='total exports', color='cotton:wheat', labels={
+             'state': 'รัฐ', 'total exports': 'ยอดส่งออกรวม'})
+fig.update_layout(xaxis_tickangle=-45)
+fig.show()
